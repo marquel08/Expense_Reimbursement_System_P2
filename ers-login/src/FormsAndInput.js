c903405d0ab1ab4030ce1c15fe86abbc4eda1e39
Desiree Steffen <desireesteffen3@gmail.com>
2022-04-06 18:54:57 -0400

    changing login form to accept user input

--- ers-login/src/FormsAndInput.js
+++ ers-login/src/FormsAndInput.js
@@ -1,5 +1,20 @@
 import React from "react";
+import './App.css';
 
+function App(){
+return(
+    <div className='wrapper'>
+        <h1>Please fill out the Below form for Reimbursments</h1>
+        <form>
+            <fieldset>
+                <label>
+                    <p>First Name</p>
+                </label>
+            </fieldset>
+        </form>
+    </div>
+)
+}
 
 
 
