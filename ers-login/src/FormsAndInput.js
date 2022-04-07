0c54c4702c0dbb704612632dcf5b4bc5e7dc9925
Desiree Steffen <desireesteffen3@gmail.com>
2022-04-06 20:02:32 -0400

    cant figure out why lastname is undefined

--- ers-login/src/FormsAndInput.js
+++ ers-login/src/FormsAndInput.js
@@ -1,20 +1,67 @@
-import React from "react";
+import React, {useReducer, useState} from "react";
 import './App.css';
 
-function App(){
+const formReducer = (state, event) => {
+    return{
+        ...state,
+        [event.name]: event.value
+    }
+}
+
+function EmpReim(){
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState(false);
+    const handleSubmit = event => {
+        event.preventDefault();
+        // alert('Revature Thanks you for Submiting this Request!')
+        setSubmitting(true);
+
+        setTimeout(() => {
+            setSubmitting(false);
+        }, 3000)
+    }
+    const handleChange = event => {
+        setFormData({fname: event.target.fname,
+        value: event.target.value, lname: event.target.lname, value: event.target.value,
+    });
+}
 return(
     <div className='wrapper'>
         <h1>Please fill out the Below form for Reimbursments</h1>
-        <form>
+        {submitting && <div>You are Submitting the Following Information:
+            <ul>
+                {Object.entries(formData).map(([fname, value]) => (
+                    <li key={fname}><strong>{fname}</strong>:{value.toString()}</li>
+                    
+                ))}
+            </ul>
+            <ul>
+                {Object.entries(formData).map(([lname, value]) => (
+                    <li key={lname}><strong>{lname}</strong>:{value.toString()}</li>
+                    
+                ))}
+            </ul>
+        </div>
+        }
+        <form onSubmit={handleSubmit}>
             <fieldset>
                 <label>
                     <p>First Name</p>
+                    <input fname="fname" onChange={handleChange}value={formData.fname } />
+                </label>
+                <label>
+                    <p>Last Name</p>
+                    <input lname="lname" onChange={handleChange} value={formData.lname }/>
                 </label>
             </fieldset>
+
+
+            <button type="submit">Submit</button>
         </form>
     </div>
 )
 }
+export default EmpReim;
 
 
 
