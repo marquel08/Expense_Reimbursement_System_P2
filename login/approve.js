e60b7d04f3a635e0cf6d7c2ff5e153ca6e64dbb3
Marquel Ellis <100174037+marquel08@users.noreply.github.com>
2022-04-17 19:27:07 -0500

    added deny button and updated css

--- login/approve.js
+++ login/approve.js
@@ -10,7 +10,7 @@ class App extends React.Component {
                 <button enabled={this.state.enabled}>Approve</button>
             </form>
             <form>
-                <button disabled={this.state.disabled}>Approve</button>
+                <button disabled={this.state.disabled}>Deny</button>
             </form>
         </div>
     }
